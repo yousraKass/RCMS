3501c733925eaded0636b86d4a5cbaa976c183a5
AbderrahmeneTliba01010 <146717533+AbderrahmeneTliba01010@users.noreply.github.com>
2023-12-10 15:40:52 +0100

    Add files via upload

--- DailySalesAndCosts.h
+++ DailySalesAndCosts.h
@@ -15,6 +15,7 @@ class Sales_and_Costs{
     void reportOnsales(int month,int year,int startYear) const;
     void reportOnsales(int day1,int month1,int year1,int day2,int month2,int year2,int startYear) const;
     void Add_Sales_and_Costs(int year,int month,int day,float sA,float sS,float sC,float sI,float sE,float pub_Cost,float gen_Cost,int startYear);
+
     private:
     struct Sales_Costs
         {
@@ -25,14 +26,14 @@ class Sales_and_Costs{
 
     struct Days
         {
-            Sales_Costs* days[31];
+            Sales_Costs days[31];
             float MonthlySales[5];
         };
     struct Months
         {
-            Days* months[12];
+            Days months[12];
         };
-    vector<Months*> Years;
+    vector<Months> Years;
 
 }
 ;
@@ -50,11 +51,11 @@ void Sales_and_Costs::reportOnsales(int month,int year,int startYear) const{
             float sum1 = 0, sum2 = 0 , sum3 = 0, sum4 = 0, sum5 = 0;
             for (int i = 0; i < 31; i++)
             {
-                sum1+= Years[year-startYear]->months[month-1]->days[i]->sales[0];
-                sum2+= Years[year-startYear]->months[month-1]->days[i]->sales[1];
-                sum3+= Years[year-startYear]->months[month-1]->days[i]->sales[2];
-                sum4+= Years[year-startYear]->months[month-1]->days[i]->sales[3];
-                sum5+= Years[year-startYear]->months[month-1]->days[i]->sales[4];
+                sum1+= Years[year-startYear].months[month-1].days[i].sales[0];
+                sum2+= Years[year-startYear].months[month-1].days[i].sales[1];
+                sum3+= Years[year-startYear].months[month-1].days[i].sales[2];
+                sum4+= Years[year-startYear].months[month-1].days[i].sales[3];
+                sum5+= Years[year-startYear].months[month-1].days[i].sales[4];
             }
             cout<<"Monthly sales of the Algerian cuisine : "<<sum1<<endl;
             cout<<"Monthly sales of the Syrian cuisine : "<<sum2<<endl;
@@ -80,11 +81,11 @@ void Sales_and_Costs::reportOnsales(int day1,int month1,int year1,int day2,int m
                     break;
                 else
                 {
-                    sum1+= Years[year1-startYear]->months[month1-1]->days[day1-1]->sales[0];
-                    sum2+= Years[year1-startYear]->months[month1-1]->days[day1-1]->sales[1];
-                    sum3+= Years[year1-startYear]->months[month1-1]->days[day1-1]->sales[2];
-                    sum4+= Years[year1-startYear]->months[month1-1]->days[day1-1]->sales[3];
-                    sum5+= Years[year1-startYear]->months[month1-1]->days[day1-1]->sales[4];
+                    sum1+= Years[year1-startYear].months[month1-1].days[day1-1].sales[0];
+                    sum2+= Years[year1-startYear].months[month1-1].days[day1-1].sales[1];
+                    sum3+= Years[year1-startYear].months[month1-1].days[day1-1].sales[2];
+                    sum4+= Years[year1-startYear].months[month1-1].days[day1-1].sales[3];
+                    sum5+= Years[year1-startYear].months[month1-1].days[day1-1].sales[4];
                 }
                 day1++;
                 if(day1 == 32)
@@ -109,28 +110,28 @@ void Sales_and_Costs::reportOnsales(int day1,int month1,int year1,int day2,int m
 }
 
 void Sales_and_Costs::GetMonthlySales(int month,int year,float &A, float &S, float &C, float &I, float &E,int startYear) const{
-    A = Years[year-startYear]->months[month-1]->MonthlySales[0];
-    S = Years[year-startYear]->months[month-1]->MonthlySales[1];
-    C = Years[year-startYear]->months[month-1]->MonthlySales[2];
-    I = Years[year-startYear]->months[month-1]->MonthlySales[3];
-    E = Years[year-startYear]->months[month-1]->MonthlySales[4];
+    A = Years[year-startYear].months[month-1].MonthlySales[0];
+    S = Years[year-startYear].months[month-1].MonthlySales[1];
+    C = Years[year-startYear].months[month-1].MonthlySales[2];
+    I = Years[year-startYear].months[month-1].MonthlySales[3];
+    E = Years[year-startYear].months[month-1].MonthlySales[4];
 }
 
 
 void Sales_and_Costs::Add_Sales_and_Costs(int year,int month,int day,float sA,float sS,float sC,float sI,float sE,float pub_Cost,float gen_Cost,int startYear)
 {
-    Years[year-startYear]->months[month-1]->days[day-1]->sales[0] = sA;
-    Years[year-startYear]->months[month-1]->days[day-1]->sales[1] = sS;
-    Years[year-startYear]->months[month-1]->days[day-1]->sales[2] = sC;
-    Years[year-startYear]->months[month-1]->days[day-1]->sales[3] = sI;
-    Years[year-startYear]->months[month-1]->days[day-1]->sales[4] = sE;
-    Years[year-startYear]->months[month-1]->days[day-1]->publicity_costs = pub_Cost;
-    Years[year-startYear]->months[month-1]->days[day-1]->general_costs = gen_Cost;
+    Years[year-startYear].months[month-1].days[day-1].sales[0] = sA;
+    Years[year-startYear].months[month-1].days[day-1].sales[1] = sS;
+    Years[year-startYear].months[month-1].days[day-1].sales[2] = sC;
+    Years[year-startYear].months[month-1].days[day-1].sales[3] = sI;
+    Years[year-startYear].months[month-1].days[day-1].sales[4] = sE;
+    Years[year-startYear].months[month-1].days[day-1].publicity_costs = pub_Cost;
+    Years[year-startYear].months[month-1].days[day-1].general_costs = gen_Cost;
 }
 
 
 float Sales_and_Costs::GetMonthlySalesOfRestaurant(int month,int year,int startYear)const{
-    return Years[year-startYear]->months[month-1]->MonthlySales[0]  +  Years[year-startYear]->months[month-1]->MonthlySales[1]  +  Years[year-startYear]->months[month-1]->MonthlySales[2]  +  Years[year-startYear]->months[month-1]->MonthlySales[3]  +  Years[year-startYear]->months[month-1]->MonthlySales[4];
+    return Years[year-startYear].months[month-1].MonthlySales[0]  +  Years[year-startYear].months[month-1].MonthlySales[1]  +  Years[year-startYear].months[month-1].MonthlySales[2]  +  Years[year-startYear].months[month-1].MonthlySales[3]  +  Years[year-startYear].months[month-1].MonthlySales[4];
 }
 
 
--- Rating.h
+++ Rating.h
@@ -1,85 +1,92 @@
 #ifndef RATING_H
 #define RATING_H
 
-#include<vector>
+#include<iostream>
+#include <vector>
 using std::vector;
+using std::cout;
+using std::endl;
 
-struct Rating_and_AOP{
-    float Rating[6];
-    float Amount_of_Prize[5];
-};
 
-struct Months
+class Rating
 {
-    Rating_and_AOP* months[12];
+public:
+    void AddRating(int month, int year, float rA, float rS, float rC, float rI, float rE, int startYear, int startMonth);
+    float GetRestaurantRating(int month, int year, int startYear) const;
+    void GetAmountOfPrize(int month, int year, float &aA, float &aS, float &aC, float &aI, float &aE, int startYear) const;
+
+    Rating() : Years(20)
+    {
+    }
+
+private:
+struct Rating_and_AOP
+    {
+        float Rating[6];
+        float Amount_of_Prize[5];
+    };
+
+    struct Months
+    {
+        Rating_and_AOP months[12];
+    };
+    
+    std::vector<Months> Years;
 };
-
-
-class Rating{
-    public:
-
-    void AddRating(int month,int year,float rA,float rS,float rC, float rI, float rE,int startYear);
-    float GetRestaurantRating(int month,int year,int startYear)const;
-    void GetAmountOfPrize(int month,int year,float &aA,float &aS,float &aC,float &aI,float &aE,int startYear)const;
-
-
-
-
-    private:
-    std::vector<Months*> Years;
-};
-
 
 // Implementation
 
-void Rating::AddRating(int month,int year,float rA,float rS,float rC, float rI, float rE,int startYear){
-    Years[year-startYear]->months[month-1]->Rating[0] = rA;
-    Years[year-startYear]->months[month-1]->Rating[1] = rS;
-    Years[year-startYear]->months[month-1]->Rating[2] = rC;
-    Years[year-startYear]->months[month-1]->Rating[3] = rI;
-    Years[year-startYear]->months[month-1]->Rating[4] = rE;
-    Years[year-startYear]->months[month-1]->Rating[5] = rA + rS + rC + rI + rE;
+void Rating::AddRating(int month, int year, float rA, float rS, float rC, float rI, float rE, int startYear, int startMonth)
+{
+    cout << "added 1" << endl;
 
-    if(year == startYear && month == 1)
+    Years[year - startYear].months[month - 1].Rating[0] = rA;
+    Years[year - startYear].months[month - 1].Rating[1] = rS;
+    Years[year - startYear].months[month - 1].Rating[2] = rC;
+    Years[year - startYear].months[month - 1].Rating[3] = rI;
+    Years[year - startYear].months[month - 1].Rating[4] = rE;
+    Years[year - startYear].months[month - 1].Rating[5] = (rA + rS + rC + rI + rE) / 5;
+
+    if (year == startYear && month == startMonth)
     {
-        Years[0]->months[0]->Amount_of_Prize[0] = rA/100;
-        Years[0]->months[0]->Amount_of_Prize[1] = rS/100;
-        Years[0]->months[0]->Amount_of_Prize[2] = rC/100;
-        Years[0]->months[0]->Amount_of_Prize[3] = rI/100;
-        Years[0]->months[0]->Amount_of_Prize[4] = rE/100;
+        Years[0].months[startMonth - 1].Amount_of_Prize[0] = rA / 100;
+        Years[0].months[startMonth - 1].Amount_of_Prize[1] = rS / 100;
+        Years[0].months[startMonth - 1].Amount_of_Prize[2] = rC / 100;
+        Years[0].months[startMonth - 1].Amount_of_Prize[3] = rI / 100;
+        Years[0].months[startMonth - 1].Amount_of_Prize[4] = rE / 100;
     }
 
-    else if(month == 1)
+    else if (month == 1)
     {
-        Years[year-startYear]->months[month-1]->Amount_of_Prize[0] = Years[year-startYear-1]->months[11]->Amount_of_Prize[0]+(rA/100);
-        Years[year-startYear]->months[month-1]->Amount_of_Prize[1] = Years[year-startYear-1]->months[11]->Amount_of_Prize[1]+(rS/100);
-        Years[year-startYear]->months[month-1]->Amount_of_Prize[2] = Years[year-startYear-1]->months[11]->Amount_of_Prize[2]+(rC/100);
-        Years[year-startYear]->months[month-1]->Amount_of_Prize[3] = Years[year-startYear-1]->months[11]->Amount_of_Prize[3]+(rI/100);
-        Years[year-startYear]->months[month-1]->Amount_of_Prize[4] = Years[year-startYear-1]->months[11]->Amount_of_Prize[4]+(rE/100);
+        Years[year - startYear].months[month - 1].Amount_of_Prize[0] = Years[year - startYear - 1].months[11].Amount_of_Prize[0] + (rA / 100);
+        Years[year - startYear].months[month - 1].Amount_of_Prize[1] = Years[year - startYear - 1].months[11].Amount_of_Prize[1] + (rS / 100);
+        Years[year - startYear].months[month - 1].Amount_of_Prize[2] = Years[year - startYear - 1].months[11].Amount_of_Prize[2] + (rC / 100);
+        Years[year - startYear].months[month - 1].Amount_of_Prize[3] = Years[year - startYear - 1].months[11].Amount_of_Prize[3] + (rI / 100);
+        Years[year - startYear].months[month - 1].Amount_of_Prize[4] = Years[year - startYear - 1].months[11].Amount_of_Prize[4] + (rE / 100);
     }
     else
     {
-        Years[year-startYear]->months[month-1]->Amount_of_Prize[0] = Years[year-startYear]->months[month-2]->Amount_of_Prize[0]+(rA/100);
-        Years[year-startYear]->months[month-1]->Amount_of_Prize[1] = Years[year-startYear]->months[month-2]->Amount_of_Prize[1]+(rS/100);
-        Years[year-startYear]->months[month-1]->Amount_of_Prize[2] = Years[year-startYear]->months[month-2]->Amount_of_Prize[2]+(rC/100);
-        Years[year-startYear]->months[month-1]->Amount_of_Prize[3] = Years[year-startYear]->months[month-2]->Amount_of_Prize[3]+(rI/100);
-        Years[year-startYear]->months[month-1]->Amount_of_Prize[4] = Years[year-startYear]->months[month-2]->Amount_of_Prize[4]+(rE/100); 
+        Years[year - startYear].months[month - 1].Amount_of_Prize[0] = Years[year - startYear].months[month - 2].Amount_of_Prize[0] + (rA / 100);
+        Years[year - startYear].months[month - 1].Amount_of_Prize[1] = Years[year - startYear].months[month - 2].Amount_of_Prize[1] + (rS / 100);
+        Years[year - startYear].months[month - 1].Amount_of_Prize[2] = Years[year - startYear].months[month - 2].Amount_of_Prize[2] + (rC / 100);
+        Years[year - startYear].months[month - 1].Amount_of_Prize[3] = Years[year - startYear].months[month - 2].Amount_of_Prize[3] + (rI / 100);
+        Years[year - startYear].months[month - 1].Amount_of_Prize[4] = Years[year - startYear].months[month - 2].Amount_of_Prize[4] + (rE / 100);
     }
-    
-    
+    cout << "added 2" << endl;
 }
 
-float Rating::GetRestaurantRating(int month,int year,int startYear)const{
-    return Years[year-startYear]->months[month-1]->Rating[5];
+float Rating::GetRestaurantRating(int month, int year, int startYear) const
+{
+    return Years[year - startYear].months[month - 1].Rating[5];
 }
 
-void Rating::GetAmountOfPrize(int month,int year,float &aA,float &aS,float &aC,float &aI,float &aE,int startYear)const{
-    aA = Years[year-startYear]->months[month-1]->Amount_of_Prize[0];
-    aS = Years[year-startYear]->months[month-1]->Amount_of_Prize[1];
-    aC = Years[year-startYear]->months[month-1]->Amount_of_Prize[2];
-    aI = Years[year-startYear]->months[month-1]->Amount_of_Prize[3];
-    aE = Years[year-startYear]->months[month-1]->Amount_of_Prize[4];
+void Rating::GetAmountOfPrize(int month, int year, float &aA, float &aS, float &aC, float &aI, float &aE, int startYear) const
+{
+    aA = Years[year - startYear].months[month - 1].Amount_of_Prize[0];
+    aS = Years[year - startYear].months[month - 1].Amount_of_Prize[1];
+    aC = Years[year - startYear].months[month - 1].Amount_of_Prize[2];
+    aI = Years[year - startYear].months[month - 1].Amount_of_Prize[3];
+    aE = Years[year - startYear].months[month - 1].Amount_of_Prize[4];
 }
-
 
 #endif
