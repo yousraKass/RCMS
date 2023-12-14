070d1d3897ebd8d5a03e96084fb4e96af93b7c8d
AbderrahmeneTliba01010 <146717533+AbderrahmeneTliba01010@users.noreply.github.com>
2023-12-14 21:31:17 +0100

    Update RESTAURANT.h

--- RESTAURANT.h
+++ RESTAURANT.h
@@ -9,8 +9,6 @@
 using namespace std;
 class AVLRestaurantTree;
 class RestaurantTree;
-class Prize_Winners;
-//enum type that indicates if the restaurant is owned/franchised
 enum Type
 {
     owned,
@@ -18,18 +16,16 @@ enum Type
 };
 class Restaurant
 {
-    //declare friend class that they need direct access to restaurant's private data members
     friend class RestaurantTree;
-    friend class Sales_and_Costs;
+    friend class SalesAndCosts;
     friend class Rating;
     friend class AVLRestaurantTree;
-    friend class Prize_Winners;
-//declaring private data members of the class Restaurant
+
 private:
     Type info;
     string name;
     int ID;
-    Sales_and_Costs SalesAndCosts;
+    SalesAndCosts Sales_and_Costs;
     Rating rating;
     Date creationDate;
     int employee_num;
@@ -38,64 +34,55 @@ private:
     int heightNode;
 
 public:
-   // constructor
-    Restaurant(const string &T, const string &Name,const int &id, const Date &creation, const int &employee);
-    // Return the type of the restaurant
+    // SALES AND COSTS TREATED IN THEIR CLASSES
+    // SALES AND COSTS TREATED IN THEIR CLASSES
+    Restaurant(string T, string Name, int id, const Date &creation, const int &employee);
+
+    // have to check what it will return (to be done while testing)
     string getType() const;
-     //return the number of employees in the restaurant
+
     int getEmployeeNumber() const;
-    //return the restaurant's name
+
     string getName() const;
-    //return the restaurant's Id
+
     int getId() const;
-    //return the restaurant's sales and costs (object)
-    Sales_and_Costs getSalesCosts() const;
-    //return the restaurant's rating (object)
+
+    SalesAndCosts getSalesCosts() const;
+
     Rating getRating() const;
-    //return the restaurant's creation date (object)
+
     Date getCreationDate() const;
 
     // show the restaurant 's data
     void showRestaurantData() const;
-    //overloading the operators ==,<,>
     bool operator==(const Restaurant &R);
     bool operator<(const Restaurant &R);
     bool operator>(const Restaurant &R);
-    //return the restaurant's Monthly ratio of the sales over the publicity cost in mm/yyyy
+    
     void getMonthlyRatio(int month, int year) const;
-    //return the restaurant's Monthly ratio of the sales over the publicity cost in a specific period of time
     void getMonthlyRatioPeriod(int startMonth, int startYear, int endMonth, int endYear) const;
-    //report on sales of the restaurant in a given month and year (calls a function of the sales and costs object)
     void reportOnsales(int month, int year) const;
-    //report on sales of the restaurant in a given period (calls a function of the sales and costs object)
-    void reportOnsales(int day1, int month1, int year1, int day2, int month2, int year2) const;
-    //add the restaurant's rating from the creation date till now
+        void reportOnsales(int day1, int month1, int year1, int day2, int month2, int year2) const;
+
+    //void reportOnsales(Date start, Date end);
     void addRating(int month, int year, float rA, float rS, float rC, float rI, float rE);
-    //return the monthly rating of a restaurant (average of the five cuisine)
     float getRestaurantRating(int month, int year);
-    //get the prize Amount of a specific month
     void getAmountOfPrize(int month, int year, float &aA, float &aS, float &aC, float &aI, float &aE);
-    //show the sales of a specific month for each cuisine and the restaurant
     void getMonthlySales(int month, int year, float &A, float &S, float &C, float &I, float &E) const;
-    //get the sales of the restaurant on a given month
     float getMonthlySalesOfRestaurant(int month, int year) const ;
-     //add the restaurant's daily sales , daily costs from the creation date till now
     void add_Sales_and_Costs(int year, int month, int day, float sA, float sS, float sC, float sI, float sE, float pub_Cost, float gen_Cost);
 };
-
-
 void Restaurant::getMonthlyRatioPeriod(int startMonth, int startYear, int endMonth, int endYear) const
 {
-    SalesAndCosts.Ratio(startMonth, startYear, endMonth, endYear, creationDate.getYear());
+    Sales_and_Costs.Ratio(startMonth, startYear, endMonth, endYear, creationDate.getYear());
 }
-
 void Restaurant::getMonthlyRatio(int month, int year) const
 {
-    SalesAndCosts.Ratio(month, year, creationDate.getYear());
+    Sales_and_Costs.Ratio(month, year, creationDate.getYear());
 }
 
-Restaurant::Restaurant(const string &T, const string &Name,const int &id, const Date &creation, const int &employee){
-
+Restaurant::Restaurant(string T, string Name, int id, const Date &creation, const int &employee) : ID(id), name(Name), creationDate(creation), employee_num(employee), heightNode(0)
+{
     if (T == "Owned")
     {
         info = owned;
@@ -108,6 +95,7 @@ Restaurant::Restaurant(const string &T, const string &Name,const int &id, const 
     rightChild = nullptr;
 }
 
+// have to check what it will return (to be done while testing)
 string Restaurant::getType() const
 {
     return (info == 0 ? "Owned " : "Franchised");
@@ -128,9 +116,9 @@ int Restaurant::getId() const
     return ID;
 }
 
-Sales_and_Costs Restaurant::getSalesCosts() const
+SalesAndCosts Restaurant::getSalesCosts() const
 {
-    return SalesAndCosts;
+    return Sales_and_Costs;
 }
 
 Rating Restaurant::getRating() const
@@ -168,6 +156,13 @@ bool Restaurant::operator>(const Restaurant &R)
     return this->getId() > R.getId();
 }
 
+
+
+/*void Restaurant::reportOnsales(Date start, Date end)
+{
+    this->SalesAndCosts.reportOnsales(start.getDay(), start.getMonth(), start.getYear(), end.getDay(), end.getMonth(), end.getYear(), creationDate.getYear());
+}*/
+
 void Restaurant::addRating(int month, int year, float rA, float rS, float rC, float rI, float rE)
 {
     int sYear = creationDate.getYear();
@@ -184,37 +179,44 @@ float Restaurant::getRestaurantRating(int month, int year)
 void Restaurant::getAmountOfPrize(int month, int year, float &aA, float &aS, float &aC, float &aI, float &aE)
 {
     int sYear = creationDate.getYear();
+    float A,S,C,I,E;
     rating.GetAmountOfPrize(month, year, aA, aS, aC, aI, aE, sYear);
+    Sales_and_Costs.GetCumulativeSales(month,year,A,S,C,I,E,sYear);
+    aA+=A;
+    aS+=S;
+    aC+=C;
+    aI+=I;
+    aE+=E;
 }
 
 void Restaurant::getMonthlySales(int month, int year, float &A, float &S, float &C, float &I, float &E) const
 {
     int sYear = creationDate.getYear();
-    SalesAndCosts.GetMonthlySales(month, year, A, S, C, I, E, sYear);
+    Sales_and_Costs.GetMonthlySales(month, year, A, S, C, I, E, sYear);
 }
 
 float Restaurant::getMonthlySalesOfRestaurant(int month, int year) const
 {
     int sYear = creationDate.getYear();
-    return SalesAndCosts.GetMonthlySalesOfRestaurant(month,year,sYear);
+    return Sales_and_Costs.GetMonthlySalesOfRestaurant(month,year,sYear);
 }
 
 void Restaurant::reportOnsales(int month, int year) const
 {
     int sYear = creationDate.getYear();
-    SalesAndCosts.reportOnsales(month, year, sYear);
+    Sales_and_Costs.reportOnsales(month, year, sYear);
 }
 
 void Restaurant::reportOnsales(int day1, int month1, int year1, int day2, int month2, int year2) const
 {
     int sYear = creationDate.getYear();
-    SalesAndCosts.reportOnsales(day1, month1, year1, day2, month2, year2, sYear);
+    Sales_and_Costs.reportOnsales(day1, month1, year1, day2, month2, year2, sYear);
 }
 
 void Restaurant::add_Sales_and_Costs(int year, int month, int day, float sA, float sS, float sC, float sI, float sE, float pub_Cost, float gen_Cost)
 {
     int sYear = creationDate.getYear();
-    SalesAndCosts.Add_Sales_and_Costs(year, month, day, sA, sS, sC, sI, sE, pub_Cost, gen_Cost, sYear);
+    Sales_and_Costs.Add_SalesAndCosts(year, month, day, sA, sS, sC, sI, sE, pub_Cost, gen_Cost, sYear);
 }
 
 #endif
